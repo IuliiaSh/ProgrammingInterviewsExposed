a0cde8911a40f1d5385d8e8c72ab0a02a435e1ec
iuliia <shevchuk.iuliia@gmail.com>
2018-10-13 09:48:23 +0200

    Recursion: String Permutations

---
+++ Recursion/StringPermutations.java
@@ -0,0 +1,33 @@
+package Recursion;
+
+public class StringPermutations {
+    public static void main(String[] args) {
+        String s = "abc";
+        findPermutations(s);
+    }
+
+    private static void findPermutations(String s) {
+        findPermutations(new StringBuilder(), new StringBuilder(s));
+    }
+
+    private static void findPermutations(StringBuilder currentResult, StringBuilder leftChars) {
+        if (leftChars.length() == 0) {
+            // move just before if to obtain permutations of all possible length
+            System.out.println(currentResult.toString());
+            return;
+        }
+
+        char nextChar;
+        for (int i = 0; i < leftChars.length(); i++) {
+            nextChar = leftChars.charAt(i);
+            // uncomment to obtain combinations
+            // if (currentResult.length() == 0 || nextChar >= currentResult.charAt(currentResult.length() - 1)) {
+                currentResult.append(nextChar);
+                leftChars.deleteCharAt(i);
+                findPermutations(currentResult, leftChars);
+                currentResult.deleteCharAt(currentResult.length() - 1);
+                leftChars.insert(i, nextChar);
+            // }
+        }
+    }
+}

2098169d595cf51eaf7704efdd5fdeff31da41d6
iuliia <shevchuk.iuliia@gmail.com>
2018-10-17 08:51:16 +0200

    ArraysAndStrings: Find First Nonrepeated Character

---
+++ ArraysAndStrings/FindFirstNonrepeatedCharacter.java
@@ -0,0 +1,24 @@
+package ArraysAndStrings;
+
+import java.util.HashMap;
+
+public class FindFirstNonrepeatedCharacter {
+    public static Character find(String s) {
+        HashMap<Character, Integer> charCountMap = new HashMap();
+        for (int i = 0; i < s.length(); i++) {
+            charCountMap.putIfAbsent(s.charAt(i), 0);
+            charCountMap.compute(s.charAt(i), (key, value) -> value + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (charCountMap.get(s.charAt(i)) == 1) {
+                return s.charAt(i);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(find("teeter"));
+    }
+}

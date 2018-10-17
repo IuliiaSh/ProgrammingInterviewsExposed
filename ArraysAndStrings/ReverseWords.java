214efd0a097e6d261705b605e1c65834880aa23c
iuliia <shevchuk.iuliia@gmail.com>
2018-10-17 09:10:00 +0200

    ArraysAndStrings: Reverse Words

---
+++ ArraysAndStrings/ReverseWords.java
@@ -0,0 +1,17 @@
+package ArraysAndStrings;
+
+public class ReverseWords {
+    public static String reverseWords(String s) {
+        String[] words = s.split("\\ ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("Do or do not, there is no try."));
+    }
+}
